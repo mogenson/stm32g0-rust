78cfd7734aa077d2175c685656a794ecd9fcc225
Michael Mogenson <michael.mogenson@gmail.com>
2020-02-16 16:11:13 -0500

    work on stm32g0 nucleo board
    Note: don't change PF2 pin from NRST to GPIO or else SWD debugging and
    flashing stops working.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 #![no_main]
 #![no_std]
 
+#[cfg(not(debug_assertions))]
 extern crate panic_halt;
+#[cfg(debug_assertions)]
+extern crate panic_semihosting;
+
 extern crate stm32g0xx_hal as hal;
 
 use hal::exti;
-use hal::gpio::{gpioa, Output, PushPull, SignalEdge};
+use hal::gpio::{gpioc, Output, PushPull, SignalEdge};
 use hal::prelude::*;
 use hal::rcc;
-use hal::stm32::{EXTI, TIM2, USART1};
+use hal::rcc::ResetMode;
+use hal::stm32::{EXTI, TIM2, USART2};
 use hal::time::Hertz;
 use hal::timer::Timer;
 use hal::{serial, serial::Rx, serial::Tx};
@@ -33,12 +38,12 @@ macro_rules! log {
 #[app(device = hal::stm32, peripherals = true)]
 const APP: () = {
     struct Resources {
-        led: gpioa::PA12<Output<PushPull>>,
+        led: gpioc::PC6<Output<PushPull>>,
         timer: Timer<TIM2>,
         period: Hertz,
-        exti: EXTI,
-        rx: Rx<USART1>,
-        tx: Tx<USART1>,
+        //exti: EXTI,
+        rx: Rx<USART2>,
+        tx: Tx<USART2>,
         producer: Producer<'static, u8, U4, u8>,
         consumer: Consumer<'static, u8, U4, u8>,
     }
@@ -48,25 +53,27 @@ const APP: () = {
         log!("init\n");
 
         let mut rcc = ctx.device.RCC.freeze(rcc::Config::pll()); // 64 MHz
-        let mut exti = ctx.device.EXTI;
+                                                                 //let mut exti = ctx.device.EXTI;
         let gpioa = ctx.device.GPIOA.split(&mut rcc);
-        let gpiob = ctx.device.GPIOB.split(&mut rcc);
+        let gpioc = ctx.device.GPIOC.split(&mut rcc);
+        //let gpiof = ctx.device.GPIOF.split(&mut rcc);
 
-        let led = gpioa.pa12.into_push_pull_output();
-        let _btn = gpioa
-            .pa0
-            .listen(SignalEdge::Falling, &mut exti)
-            .into_pull_up_input();
+        let led = gpioc.pc6.into_push_pull_output();
+
+        // let _btn = gpiof
+        //     .pf2
+        //     .listen(SignalEdge::Falling, &mut exti)
+        //     .into_pull_up_input();
 
         static mut Q: Queue<u8, U4, u8> = Queue(ConstQueue::u8());
         let (producer, consumer) = unsafe { Q.split() };
 
         let (tx, mut rx) = ctx
             .device
-            .USART1
+            .USART2
             .usart(
-                gpioa.pa9,
-                gpiob.pb7,
+                gpioa.pa2,
+                gpioa.pa3,
                 serial::Config::default().baudrate(115200.bps()),
                 &mut rcc,
             )
@@ -83,7 +90,7 @@ const APP: () = {
             led: led,
             timer: timer,
             period: period,
-            exti: exti,
+            //exti: exti,
             rx: rx,
             tx: tx,
             producer: producer,
@@ -110,21 +117,24 @@ const APP: () = {
         ctx.resources.timer.clear_irq();
     }
 
-    #[task(binds = EXTI0_1, resources = [exti, timer, period])]
-    fn button(ctx: button::Context) {
-        log!("button\n");
-        let period = ctx.resources.period;
-        if *period == 1.hz() {
-            *period = 4.hz();
-            ctx.resources.timer.start(*period);
-        } else {
-            *period = 1.hz();
-            ctx.resources.timer.start(*period);
-        }
-        ctx.resources.exti.unpend(exti::Event::GPIO0);
-    }
+    // #[task(binds = EXTI2_3, resources = [exti, timer, period])]
+    // fn button(ctx: button::Context) {
+    //     log!("button\n");
+    //     let period = ctx.resources.period;
+    //     if *period == 1.hz() {
+    //         *period = 4.hz();
+    //         ctx.resources.timer.start(*period);
+    //     } else {
+    //         *period = 1.hz();
+    //         ctx.resources.timer.start(*period);
+    //     }
+    //     ctx.resources.exti.unpend(exti::Event::GPIO2);
+    //     //let _rpr1 = &ctx.resources.exti.rpr1;
+    //     let _fpr1 = &ctx.resources.exti.fpr1;
+    //     let _x = 5;
+    // }
 
-    #[task(binds = USART1, resources = [rx, producer])]
+    #[task(binds = USART2, resources = [rx, producer])]
     fn rx(ctx: rx::Context) {
         if let Ok(byte) = block!(ctx.resources.rx.read()) {
             ctx.resources.producer.enqueue(byte).ok();
